9ed99cda69ce502b276d8190215d8b68c6a6f4f3
adhithyaaa2424 <adhithyaaa2424@gmail.com>
2026-02-11 18:39:29 +0530

    Restore original backend from team-one

---
+++ backend/src/test/java/com/example/legalmatchpro/LegalMatchProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legalmatchpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalMatchProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
