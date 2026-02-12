eddfea51a12f7d297eea9712953e499c7edead2b
adhithyaaa2424 <adhithyaaa2424@gmail.com>
2026-02-12 12:26:33 +0530

    Backend: Added UserRepository setup

--- backend/src/main/java/com/legalmatch/backend/LegalMatchProApplication.java
+++ backend/src/main/java/com/legalmatch/backend/LegalMatchProApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LegalMatchProApplication {
 
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
         SpringApplication.run(LegalMatchProApplication.class, args);
-	}
-
+    }
 }
