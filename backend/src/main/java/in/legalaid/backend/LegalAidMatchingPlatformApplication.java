209914728c64b2fd3534eccb4c0eb5759fcdc768
Harshavardhan Penta <123086106+Harshavardhan-creat@users.noreply.github.com>
2026-02-12 16:59:38 +0530

    Add files via upload

---
+++ backend/src/main/java/in/legalaid/backend/LegalAidMatchingPlatformApplication.java
@@ -0,0 +1,13 @@
+package in.legalaid.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalAidMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalAidMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/legalaid/backend/LegalAidMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package in.legalaid.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalAidMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
