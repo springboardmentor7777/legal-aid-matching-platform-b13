eb63b1ee3ef2bc56df5312de67380e27e767c0b6
sandeep-codez-7 <23981a4293@raghuenggcollege.in>
2026-02-14 18:17:22 +0530

    added DropDown

--- frontend/legal-aid-platform/src/components/Navbar.tsx
+++ frontend/legal-aid-platform/src/components/Navbar.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import type { Role } from "../pages/Dashboard";
+import Dropdown from "./Dropdown";
+// import { MoreHorizontal } from "lucide-react";
+import { FaUserCircle } from "react-icons/fa";
+import {FaSignOutAlt} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 interface NavbarProps {
   name: string;
@@ -8,6 +13,7 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ name, role, toggleSidebar }) => {
+  const navigate = useNavigate();
   return (
     <header className="bg-white border-b border-blue-100 shadow-md flex items-center p-4 sticky top-0 z-50">
       {/* Left: Legal Aid */}
@@ -32,15 +38,27 @@ const Navbar: React.FC<NavbarProps> = ({ name, role, toggleSidebar }) => {
           </svg>
         </button>
 
-        <span className="text-xl font-bold text-blue-900">LEGAL AID MATCHING PLATFORM</span>
+        <span className="text-l font-bold text-blue-900">LEGAL AID MATCHING PLATFORM</span>
       </div>
 
       {/* Right: Profile */}
-      <div className="ml-auto flex items-center space-x-4">
-        <span className="text-blue-900 font-medium">{name}</span>
-        <div className="w-10 h-10 rounded-full bg-blue-200 flex items-center justify-center text-blue-900 font-bold">
+      <div className="ml-auto flex items-center space-x-3">
+        {/* <span className="text-blue-900 font-medium">{name}</span> */}
+        {/* <div className="w-10 h-10 rounded-full bg-blue-200 flex items-center justify-center text-blue-900 font-bold">
           {name.charAt(0)}
-        </div>
+        </div> */}
+        {/* <button className="w-10 h-10 rounded-full bg-blue-200 flex items-center justify-center text-blue-900 font-bold" onClick={()=>{}}>
+          {name.charAt(0)}
+        </button> */}
+        <Dropdown items={[
+          {label:<p className="flex items-center gap-[50px]">Profile <span className="">{<FaUserCircle></FaUserCircle>}</span> </p>, onClick:()=>{navigate("/profile")}},
+          {label:<p className="flex items-center gap-[50px]">Logout <span className="">{<FaSignOutAlt></FaSignOutAlt>}</span> </p>, onClick: ()=>{navigate("/logout")}}
+          ]}>
+          <button className="w-10 h-10 rounded-full bg-blue-200 flex items-center justify-center text-blue-900 font-bold" onClick={()=>{}}>
+          {name.charAt(0)}
+          {/* <MoreHorizontal size={20} /> */}
+        </button>
+        </Dropdown>
       </div>
     </header>
   );
