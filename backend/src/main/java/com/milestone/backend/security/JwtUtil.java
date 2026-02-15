e05b156479f655954a7826c1e207218d766d1e29
mansichaudhari0106-tech <mansichaudhari0106@gmail.com>
2026-02-15 17:32:11 +0530

    Added security folder

---
+++ backend/src/main/java/com/milestone/backend/security/JwtUtil.java
@@ -0,0 +1,73 @@
+package com.milestone.backend.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "your-very-secret-key-that-is-long-enough-123456";
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    private Key getSigningKey() {
+    byte[] keyBytes = secretKey.getBytes();
+    return Keys.hmacShaKeyFor(keyBytes);
+}
+
+    public String generateRefreshToken(UserDetails userDetails) {
+    return Jwts.builder()
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 7 days
+            .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+            .compact();
+}
+
+
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
