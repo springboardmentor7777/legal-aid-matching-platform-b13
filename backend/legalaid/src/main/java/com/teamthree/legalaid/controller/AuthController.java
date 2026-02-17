e0caa5c6f9eed4192ca45e5e03a300e22066591c
devameghana <devameghana2005@gmail.com>
2026-02-17 11:01:34 +0530

    commiting the backend

---
+++ backend/legalaid/src/main/java/com/teamthree/legalaid/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.teamthree.legalaid.controller;
+
+import com.teamthree.legalaid.dto.AuthResponse;
+import com.teamthree.legalaid.dto.LoginRequest;
+import com.teamthree.legalaid.dto.RegisterRequest;
+import com.teamthree.legalaid.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
---
+++ backend/legalaid/src/main/java/com/teamthree/legalaid/security/Jwtservice.java
@@ -0,0 +1,39 @@
+package com.teamthree.legalaid.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class Jwtservice {
+    private final String SECRET_KEY = "mysecretkeymysecretkeymysecretkey";
+
+    public String generateAccessToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
