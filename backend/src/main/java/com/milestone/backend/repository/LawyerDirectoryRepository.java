e689e5d6587115d662f78451d40113ca3d3be1fd
Mansi Chaudhari <mansichaudhari0106@gmail.com>
2026-02-28 14:56:26 +0530

    Add files via upload

---
+++ backend/src/main/java/com/milestone/backend/repository/LawyerDirectoryRepository.java
@@ -0,0 +1,10 @@
+package com.milestone.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.milestone.backend.entity.LawyerDirectory;
+
+public interface LawyerDirectoryRepository
+        extends JpaRepository<LawyerDirectory, Long> {
+                boolean existsByNameAndLocation(String name, String location);
+}
