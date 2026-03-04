b59da08f81c32da4228ea7cdef45cb7ae414dca0
swati-boop <swatikh199778@gmail.com>
2026-03-04 13:04:32 +0530

    committed

--- backend/src/main/java/com/legalmatch/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/legalmatch/backend/dto/RegisterRequest.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
 
-    private String Name;
+    private String name;
     private String email;
     private String password;
     private Role role;
