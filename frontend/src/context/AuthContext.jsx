a7f0a772bdfe3b6d5e0454bcea83821134d97f76
swati-boop <swatikh199778@gmail.com>
2026-03-09 17:37:03 +0530

    committed changes for milestone 2

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://localhost:8080";
 
 const AuthContext = createContext();
 
