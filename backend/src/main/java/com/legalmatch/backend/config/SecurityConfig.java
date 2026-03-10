e0979950c36380a833a44da6e32516d9a07fed55
swati-boop <swatikh199778@gmail.com>
2026-03-10 19:57:47 +0530

    committed changes for milestone 2

--- backend/src/main/java/com/legalmatch/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/legalmatch/backend/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
