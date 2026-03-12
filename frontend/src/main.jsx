34829ec49dffbed966d3389e7e8d7ff1f4efaf52
SamyakM07 <mahajansam74@gmail.com>
2026-03-13 00:02:22 +0530

    Resolve merge conflicts in package.json, main.jsx, index.css, App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import './index.css';
->>>>>>> 2fa9e5e72acd3a0f2939a75f13b07aa7080f5b88
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
