7bcd9d978a043130a6b91a7fddc9d48f4a8043f7
swati-boop <swatikh199778@gmail.com>
2026-03-20 12:27:20 +0530

    committed changes for milestone 2

---
+++ backend/src/main/java/com/legalmatch/backend/controller/NGOController.java
@@ -0,0 +1,21 @@
+package com.legalmatch.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import com.legalmatch.backend.entity.NGO;
+import com.legalmatch.backend.service.NGOService;
+
+@RestController
+@RequestMapping("/api/ngos")
+@RequiredArgsConstructor
+public class NGOController {
+
+    private final NGOService ngoService;
+
+    @GetMapping
+    public List<NGO> getAllNGOs() {
+        return ngoService.getAllNGOs();
+    }
+}
