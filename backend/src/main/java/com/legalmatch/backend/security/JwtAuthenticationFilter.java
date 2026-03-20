4fe02ae4b8412dd091b6b77588f15d1137de965a
swati-boop <swatikh199778@gmail.com>
2026-03-20 15:37:39 +0530

    milestone 3 commits

--- backend/src/main/java/com/legalmatch/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/legalmatch/backend/security/JwtAuthenticationFilter.java
@@ -23,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
     @Override
 protected boolean shouldNotFilter(HttpServletRequest request) {
-    return request.getRequestURI().startsWith("/auth");
+    return request.getRequestURI().startsWith("/api/auth");
 }
 
     @Override
