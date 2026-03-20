34050644d60097f5813610babc543da526c55d1c
swati-boop <swatikh199778@gmail.com>
2026-03-20 14:16:12 +0530

    milestone 3 commits

--- backend/src/main/java/com/legalmatch/backend/controller/AuthController.java
+++ backend/src/main/java/com/legalmatch/backend/controller/AuthController.java
@@ -13,12 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import com.legalmatch.backend.dto.RefreshTokenRequest;
 
 @RestController
-<<<<<<< HEAD
-@RequestMapping("/auth")
+
+@RequestMapping("/api/auth")
 @CrossOrigin(origins = {"http://localhost:5173","http://localhost:5174"})
-=======
-@RequestMapping("api/auth")
->>>>>>> c43b6ec660e4ac40d75ba60fd998006ba9f96575
+
+
 @RequiredArgsConstructor
 public class AuthController {
 
