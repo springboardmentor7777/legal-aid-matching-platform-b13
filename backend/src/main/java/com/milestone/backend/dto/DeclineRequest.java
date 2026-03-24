a37cf37272834b91135f1927517f4f7a8be5492c
Mansi Chaudhari <mansichaudhari0106@gmail.com>
2026-03-24 14:21:37 +0530

    Add files via upload

---
+++ backend/src/main/java/com/milestone/backend/dto/DeclineRequest.java
@@ -0,0 +1,13 @@
+package com.milestone.backend.dto;
+
+public class DeclineRequest {
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
